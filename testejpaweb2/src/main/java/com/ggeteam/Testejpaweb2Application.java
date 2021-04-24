799f2658cb5f5a7fa4f420ab673d447c2d0cd678
Gustavo Soares <66889256+gus-soares19@users.noreply.github.com>
2021-04-24 11:44:59 -0300

    Add files via upload

--- testejpaweb2/src/main/java/com/ggeteam/Testejpaweb2Application.java
+++ testejpaweb2/src/main/java/com/ggeteam/Testejpaweb2Application.java
@@ -15,26 +15,26 @@ public class Testejpaweb2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Testejpaweb2Application.class, args);
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pizza");
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pizzas");
 
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 		Pizza pizzaAdicionar = new Pizza();
-		Pizza pizzaRemover = entityManager.find(Pizza.class, 3);
+		Pizza pizzaRemover = entityManager.find(Pizza.class, 5);
 		Pizza pizzaAtualizar = entityManager.find(Pizza.class, 4);
 
 		pizzaAdicionar.setSabor("4 Queijos");
 		pizzaAdicionar.setPreco(35);
 		pizzaAdicionar.setTamanho('G');
 
-		entityManager.getTransaction().begin();
-		entityManager.persist(pizzaAdicionar);
-		System.out.println("Pizza com sabor " + pizzaAdicionar.getSabor() + " adicionada.");
-		entityManager.remove(pizzaRemover);
+//		entityManager.getTransaction().begin();
+//		entityManager.persist(pizzaAdicionar);
+//		System.out.println("Pizza com sabor " + pizzaAdicionar.getSabor() + " adicionada.");
+//		entityManager.remove(pizzaRemover);
 		System.out.println("Pizza com sabor " + pizzaRemover.getSabor() + " removida.");
-		pizzaAtualizar.setSabor("Peperoni");
+//		pizzaAtualizar.setSabor("Peperoni");
 		System.out.println("Pizza atualizada para o sabor " + pizzaAtualizar.getSabor() + ".");
-		entityManager.getTransaction().commit();
+//		entityManager.getTransaction().commit();
 
 		entityManager.close();
 		entityManagerFactory.close();
--- testejpaweb2/src/main/java/com/ggeteam/domain/Pizza.java
+++ testejpaweb2/src/main/java/com/ggeteam/domain/Pizza.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "pizza")
+@Table(name = "pizzas")
 public class Pizza {
    
   @Id
